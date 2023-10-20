83a1719d94374f87018b605c7d4df2c57c5b2c26
Pankaj-pm <113245658+Pankaj-pm@users.noreply.github.com>
2023-10-20 09:24:40 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(){
+
+    /*const float pi=3.14;
+    int noOfMonth=2147483647; 
+    printf("%f\n",pi);
+    printf("%0.1f\n",pi);
+    printf("%d",noOfMonth);*/
+
+    /*int a,b;
+    printf("Enter value of a\n");
+    scanf("%d",&a);
+    printf("Enter value of b\n");
+    scanf("%d",&b);
+    printf("sum of  %d and %d = %d",a,b,a+b);*/
+
+    int en,hi,gu;
+
+    printf("Enter English Mark = ");
+    scanf("%d",&en);
+
+    printf("Enter Hindi Mark = ");
+    scanf("%d",&hi);
+
+    printf("Enter Gujrati Mark = ");
+    scanf("%d",&gu);
+
+    printf("\nTotal Mark is = %d",en+hi+gu);
+
+    return 0;
+}
